8f542f2298cfed13579ac1b7759eabde5f57b2ee
Rejaul Karim Tirtho <rktirtho@gmail.com>
2022-02-20 12:25:31 +0600

    project created

---
+++ src/main/java/com/rktirtho/resources/SpringBootPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.rktirtho.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rktirtho/resources/SpringBootPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rktirtho.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
